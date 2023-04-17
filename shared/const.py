77f14aa81df4296c77e0ae1895c5703e7826b6c7
ju_resplande <julianarsg13@gmail.com>
2023-04-17 10:07:37 -0300

    pure format

--- shared/const.py
+++ shared/const.py
@@ -1,37 +1,52 @@
-import json
+import jsonlines
 
 from tqdm import tqdm
 
 task_ner_labels = {
-    'ace04': ['FAC', 'WEA', 'LOC', 'VEH', 'GPE', 'ORG', 'PER'],
-    'ace05': ['FAC', 'WEA', 'LOC', 'VEH', 'GPE', 'ORG', 'PER'],
-    'scierc': ['Method', 'OtherScientificTerm', 'Task', 'Generic', 'Material', 'Metric'],
+    "ace04": ["FAC", "WEA", "LOC", "VEH", "GPE", "ORG", "PER"],
+    "ace05": ["FAC", "WEA", "LOC", "VEH", "GPE", "ORG", "PER"],
+    "scierc": [
+        "Method",
+        "OtherScientificTerm",
+        "Task",
+        "Generic",
+        "Material",
+        "Metric",
+    ],
 }
 
 task_rel_labels = {
-    'ace04': ['PER-SOC', 'OTHER-AFF', 'ART', 'GPE-AFF', 'EMP-ORG', 'PHYS'],
-    'ace05': ['ART', 'ORG-AFF', 'GEN-AFF', 'PHYS', 'PER-SOC', 'PART-WHOLE'],
-    'scierc': ['PART-OF', 'USED-FOR', 'FEATURE-OF', 'CONJUNCTION', 'EVALUATE-FOR', 'HYPONYM-OF', 'COMPARE'],
+    "ace04": ["PER-SOC", "OTHER-AFF", "ART", "GPE-AFF", "EMP-ORG", "PHYS"],
+    "ace05": ["ART", "ORG-AFF", "GEN-AFF", "PHYS", "PER-SOC", "PART-WHOLE"],
+    "scierc": [
+        "PART-OF",
+        "USED-FOR",
+        "FEATURE-OF",
+        "CONJUNCTION",
+        "EVALUATE-FOR",
+        "HYPONYM-OF",
+        "COMPARE",
+    ],
 }
 
 assert task_ner_labels.keys() == task_rel_labels.keys()
 
 tasks = set(task_ner_labels.keys()).add("custom")
 
+
 def get_ner_labels(args):
     labels = set()
 
     for split_file in [args.train_data, args.dev_data, args.test_data]:
-        with open(split_file, "r") as f:
-            data = json.load(f)
-        
-        for item in tqdm(data):
-            sample_labels = {e["type"] for e in item["entities"]}
-            labels = labels.union(sample_labels)
-    
+        with jsonlines.open(split_file, "r") as reader:
+            for item in tqdm(reader):
+                sample_labels = {e[2] for sent in item["ner"] for e in sent}
+                labels = labels.union(sample_labels)
+
     labels = list(labels)
 
     return labels
+
 
 def get_labelmap(label_list):
     label2id = {}
